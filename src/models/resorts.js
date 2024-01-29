b5f3cda73bc5c956b289620329141a4e06f93b51
LerclercDuong <89032831+LerclercDuong@users.noreply.github.com>
2024-01-29 23:35:37 +0700

    add resort model

---
+++ src/models/resorts.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const resortSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    facilities: [{
+        type: String
+    }],
+    nearby_attractions: [{
+        type: String
+    }],
+    policies: [{
+        type: String
+    }],
+    image_urls: [{
+        type: String
+    }]
+});
+
+const Resort = mongoose.model('Resorts', resortSchema);
+
+module.exports = Resort;
