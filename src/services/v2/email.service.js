8dea35225235e268cd251c1d0016da6266f258e2
bwfnguyenvu <bwfnguyenvu@gmail.com>
2024-04-04 03:22:26 +0700

    deploy

--- src/services/v2/email.service.js
+++ src/services/v2/email.service.js
@@ -19,7 +19,7 @@ class EmailService {
 
     async SendVerificationEmail(to, token) {
         const subject = 'Email Verification';
-        const verificationEmailUrl = `http://localhost:3000/email/verify-email?token=${token}`;
+        const verificationEmailUrl = `https://nicetripvn.vercel.app/email/verify-email?token=${token}`;
         const text = `Dear user,
                              To verify your email, click on this link: ${verificationEmailUrl}
                              If you did not create an account, then ignore this email.`;
@@ -28,7 +28,7 @@ class EmailService {
 
     async SendPasswordRecoveryEmail(to, username, token) {
         const subject = `Password Recovery for account ${username}`;
-        const recoveryUrl = `http://localhost:3000/reset-password?token=${token}`;
+        const recoveryUrl = `https://nicetripvn.vercel.app/reset-password?token=${token}`;
         const text = `Dear user,
                         To recover your password, click on this link: ${recoveryUrl}
                         If you did not request to change password, then ignore the email.`
@@ -136,7 +136,7 @@ class EmailService {
     async SendReservationConfirmEmail(to, reservationInfo, token) {
         const timeshareId = reservationInfo.timeshareId._id;
         const reservationId = reservationInfo._id;
-        const confirmReservationUrl = `http://localhost:3000/timeshare/${timeshareId}/reservation/${reservationId}/confirm?token=${token}`
+        const confirmReservationUrl = `https://nicetripvn.vercel.app/timeshare/${timeshareId}/reservation/${reservationId}/confirm?token=${token}`
         const subject = 'Reservation at NiceTrip';
         const text = `Thank you for your reservation at NiceTrip,
                              To confirm your reservation at post ${timeshareId}
